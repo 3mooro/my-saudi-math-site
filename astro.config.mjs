4072b8e175c4b77eab792fac5741fe75ca44b13c
Omar <omarashrah88998@gmail.com>
2026-04-11 23:57:18 +0200

    تحديث إعدادات الدومين الخاص

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
-  // ضع رابط موقعك النهائي هنا (مع اسم المستخدم واسم المستودع)
-  site: 'https://3mooro.github.io',
-  // ضع اسم المستودع هنا مع شرطة مائلة في البداية والنهاية
-  base: '/my-saudi-math-site/',
-  integrations: [githubPages()],
+  // ضع الدومين الجديد الخاص بك هنا
+  site: 'https://ksaacademy.online',
+  
+  // ⚠️ قمنا بحذف سطر base تماماً لأنه لم يعد هناك حاجة إليه مع الدومين الخاص
 });
