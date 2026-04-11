a95d067f1b5ce007be0d067234b066d4a959bf66
Omar <omarashrah88998@gmail.com>
2026-04-12 00:24:24 +0200

    إصلاح إعدادات الخطوط والمقالات

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // رابط الدومين الخاص بك
+  // رابط موقعك الرسمي
   site: 'https://ksaacademy.online',
   
-  // تفعيل إضافات القالب الأساسية
+  // تفعيل إضافات المقالات وخريطة الموقع
   integrations: [mdx(), sitemap()],
+  
+  // استعادة إعدادات الخطوط التي يبحث عنها القالب
+  fonts: [
+    {
+      name: 'Atkinson Hyperlegible',
+      provider: fontProviders.google(),
+      cssVariable: '--font-atkinson',
+    }
+  ]
 });
