b301c78b13a3d9a4199e3ff650a05455d7b7e8fa
Omar <omarashrah88998@gmail.com>
2026-09-04 02:14:27 +0300

    Fix blog schema image validation to fix 404s

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,7 @@ const blog = defineCollection({
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
+			heroImage: z.string().optional(),
 			author: z.string().default('KSA Academy'),
 			readingTime: z.string().optional(),
 			tags: z.array(z.string()).optional(),
